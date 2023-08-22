9d4b6402f2bf112a70535d621b97f6dcec0fdf0a
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-08-22 12:27:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <!DOCTYPE Html>
 <html lang="en">
   <head>
-    
+  <meta charset="UTF-8">
+  <meta name="description" content="meta tags"><!--->
+  <meta name="keywords" content="Github vitoms  catena"><!--Define keywords for search engines-->
+  <meta name="author" content="vitoms catena"> <!--Define the author of a page-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"><!--Setting the viewport to make your
+    website look good on all devices-->
+  <meta http-equiv="refresh" content="30"><!--Refresh document every 30 seconds-->
+
   </head>
   <body>
     
